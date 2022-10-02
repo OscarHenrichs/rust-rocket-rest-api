3ecdf85a8bd18227fbfb8aec2d2ac1b87e6251ab
Oscar Henrichs <oscar.henrichs.work@outlook.com>
2022-10-02 04:33:28 -0300

    serde_json

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,23 @@
 extern crate rocket;
 
 #[get("/")]
-fn index() ->&'static str {
+fn index() -> &'static str {
     "Hello, world2"
 }
 
 fn main() {
+    const PATH: &str = "data.sqlite";
+
+    {
+        let db_connection = rusqlite::Connection::open(PATH).unwrap();
+
+        db_connection
+            .execute(
+                "CREATE TABLE IF NOT EXISTS TEST (id integer primary key);",
+                [],
+            )
+            .unwrap();
+    }
+
     rocket::ignite().mount("/", routes![index]).launch();
 }
